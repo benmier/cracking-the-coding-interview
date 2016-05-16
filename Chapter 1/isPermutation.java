ed6645687229285716ae1003e2566e0d3205d2bd
benmier <bencmier@gmail.com>
2016-05-16 14:51:35 -0700

    Cracking the Coding Interview Algorithms

---
+++ Chapter 1/isPermutation.java
@@ -0,0 +1,11 @@
+public class isUnique{
+	static boolean isPermutation(String str1, String str2){
+		if(str1.length() != str2.length())
+			return false;
+		for(int i=0; i<st1.length)
+	} 
+
+	public static void main(String[] args){
+		System.out.println(isPermutation("test","estt"));
+	}
+}
