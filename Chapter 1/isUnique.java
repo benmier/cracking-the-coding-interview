0f88f74115385bbe8182604e9e7cda257e51de01
benmier <bencmier@gmail.com>
2016-05-16 14:46:31 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isUnique.java
+++ Chapter 1/isUnique.java
@@ -1,5 +1,7 @@
 public class isUnique{
 	static boolean isUniqueChars(String str){
+		if(str.length()>128 || str.length()==0) //Only 128 alphabet in ASCII
+			return false;
 		int strToInt[] = new int[128];
 		for(int i=0; i<str.length(); i++){
 			if(strToInt[str.charAt(i)]==1)
@@ -10,6 +12,6 @@ public class isUnique{
 	} 
 
 	public static void main(String[] args){
-		System.out.println(isUniqueChars("abcdefghijklmnopqrstuvwxyz"));
+		System.out.println(isUniqueChars(""));
 	}
 }
