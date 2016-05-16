48fa69e3fc5a824c1b03edb3ec6d6a1cbf25fbf6
benmier <bencmier@gmail.com>
2016-05-15 19:59:47 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isUnique.java
+++ Chapter 1/isUnique.java
@@ -1,10 +1,20 @@
 import java.util.Hashtable;
 
-
-	Hashtable<String, String> hashtable = new Hashtable<String, String>();
+class MakeHash{
+	Hashtable<Char, String> hashtable = new Hashtable<Char, String>();
+	MakeHash (String word){
+		for(int i=0; i<word.length(); i++){
+			hashtable.put(word.charAt(i), "1");
+		}
+		if(hashtable.length()!=word.length())
+			return false;
+		return true;
+	}
+}
 
 public class isUnique{
 	public static void main(String[] args){
-		
+		MakeHash test = new MakeHash("test");
+		System.out.println(test);
 	}
 }
