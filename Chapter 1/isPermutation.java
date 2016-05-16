a1de717b422fb2f3800dc89f1ba29508e699254c
benmier <bencmier@gmail.com>
2016-05-16 15:12:02 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isPermutation.java
+++ Chapter 1/isPermutation.java
@@ -3,9 +3,10 @@ public class isPermutation{
 		if(str1.length() != str2.length())
 			return false;
 		for(int i=0; i<str1.length(); i++){
-			for(int j=0; j<str2.length(); i++){
+			for(int j=0; j<str2.length(); j++){
+				// System.out.println("i: "+str1.charAt(i)+", j: "+str2.charAt(j));
 				if(str1.charAt(i)==str2.charAt(j)){
-					System.out.println(str1.charAt(i)+", "+str2.charAt(j)+", "+str2);
+					// System.out.println(str2.substring(0,j)+" + "+str2.substring(j+1));
 					str2 = str2.substring(0,j)+str2.substring(j+1);
 					break;
 				}
