6e4730a99df625fadacf552ce32448f9ea854f7d
benmier <bencmier@gmail.com>
2016-05-17 12:36:43 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isPalendromePermutation.java
+++ Chapter 1/isPalendromePermutation.java
@@ -1,7 +1,10 @@
 public class isPalendromePermutation{
-	
+	static String isPalendromePermutation(String str){
+		str = str.toLowerCase();
+		return str;
+	}
 
 	public static void main(String[] args){
-		System.out.println(urlify("Mr John Smith    "));
+		System.out.println(isPalendromePermutation("Tact Coa"));
 	}
 }
