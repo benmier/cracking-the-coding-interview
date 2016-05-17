5378aabdb95deda58a11ebb439f6a2e65a355f39
benmier <bencmier@gmail.com>
2016-05-17 12:52:01 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isPalendromePermutation.java
+++ Chapter 1/isPalendromePermutation.java
@@ -1,12 +1,27 @@
 public class isPalendromePermutation{
 	static boolean isPalendromePermutation(String str){
 		str = str.toLowerCase();
+		str = str.replaceAll("\\s+","");
+		int[] charSet = new int[128];
+		int count = 0;
 		for(int i=0; i<str.length(); i++){
-			
+			if(charSet[str.charAt(i)]>0)
+				charSet[str.charAt(i)]--;
+			else
+				charSet[str.charAt(i)]++;
 		}
+		for(int c: charSet){
+			if(c>1)
+				return false;
+			if(c==1)
+				count++;
+		}
+		if(count>1)
+			return false;
+		return true;
 	}
 
 	public static void main(String[] args){
-		System.out.println(isPalendromePermutation("Tact Coa"));
+		System.out.println(isPalendromePermutation("abcdefg gfedcbassag"));
 	}
 }
