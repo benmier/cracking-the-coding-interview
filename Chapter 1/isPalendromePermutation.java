55a5267d68957878a459fb7a33c36b024a3bb8ec
benmier <bencmier@gmail.com>
2016-05-17 12:57:06 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isPalendromePermutation.java
+++ Chapter 1/isPalendromePermutation.java
@@ -22,6 +22,6 @@ public class isPalendromePermutation{
 	}
 
 	public static void main(String[] args){
-		System.out.println(isPalendromePermutation("abcdefg gfedcbassag"));
+		System.out.println(isPalendromePermutation("aabbcs"));
 	}
 }
