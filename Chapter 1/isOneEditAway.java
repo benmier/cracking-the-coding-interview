0a092897b50860851e2dc1ec0eff03858c783d64
benmier <bencmier@gmail.com>
2016-05-17 14:23:36 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/isOneEditAway.java
+++ Chapter 1/isOneEditAway.java
@@ -1,7 +1,7 @@
 public class isOneEditAway{
 	static boolean isOneEditAway(String a, String b){
-		a = a.toLowerCase();
-		b = b.toLowerCase();
+		// a = a.toLowerCase();
+		// b = b.toLowerCase();
 		if(a.length()<b.length()){
 			String c = b;
 			b = a;
@@ -11,19 +11,17 @@ public class isOneEditAway{
 		for(int i=0; i<a.length(); i++){
 			charMap[a.charAt(i)]++;
 		}
-		int count = 0;
 		for(int i=0; i<b.length(); i++){
 			charMap[b.charAt(i)]--;
-			if(charMap[b.charAt(i)]>1 || charMap[b.charAt(i)]<-1)
-				return false;
 		}
-		// for(int c: charMap){
-		// 	count+=c;
-		// }
-		// return count==1;
+		int count = 0;
+		for(int c: charMap){
+			count+=Math.abs(c);
+		}
+		return count==1;
 	}
 
 	public static void main(String[] args){
-		System.out.println(isOneEditAway("abfd","abcdc"));
+		System.out.println(isOneEditAway("pale","Pale"));
 	}
 }
