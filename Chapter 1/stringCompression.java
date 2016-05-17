a260f322c7d2422e5953b61f7a40727ae3f75d7c
benmier <bencmier@gmail.com>
2016-05-17 16:25:32 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/stringCompression.java
+++ Chapter 1/stringCompression.java
@@ -1,6 +1,16 @@
 public class stringCompression{
 	static String stringCompression(String str){
-		
+		boolean newChar = true;
+		char[] cStr = new char[str.length()];
+		for(int i=0; i<str.length(); i++){
+			if(newChar)
+				cStr[i]=str.charAt(i);
+			else{
+				
+			}
+		}
+		String s = new String(cStr);
+		return s;
 	}
 
 
