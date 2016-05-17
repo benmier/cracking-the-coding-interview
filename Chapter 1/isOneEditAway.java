ef8e2331053868b1ecb1cc836e0032e273d215b4
benmier <bencmier@gmail.com>
2016-05-17 13:22:38 -0700

    Cracking the Coding Interview Algorithms

---
+++ Chapter 1/isOneEditAway.java
@@ -0,0 +1,10 @@
+public class isOneEditAway{
+	static boolean isOneEditAway(String a, String b){
+		str = str.toLowerCase();
+		
+	}
+
+	public static void main(String[] args){
+		System.out.println(isOneEditAway("pale","ple"));
+	}
+}
