0ffdc5a784a33c7021bac6c3ad2d6d55c0d627a4
benmier <bencmier@gmail.com>
2016-05-18 10:39:42 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/rotateMatrix.java
+++ Chapter 1/rotateMatrix.java
@@ -3,8 +3,8 @@ public class rotateMatrix{
 		for(int i=0; i<1; i++){
 			for(int j=0; j<arr[i].length; j++){
 				int temp = arr[i][j];
-				arr[i][j] = arr[arr[i].length-i-1][arr[j].length-j-1];
-				arr[arr[i].length-i-1][arr[j].length-j-1] = temp;
+				arr[i][j] = arr[arr[i].length-j-1][arr[j].length-i-1];
+				arr[arr[i].length-j-1][arr[j].length-i-1] = temp;
 			}
 		}
 
