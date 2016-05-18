55e7670db721c39636b8c51f0708c26a45248574
benmier <bencmier@gmail.com>
2016-05-18 09:38:29 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/stringCompression.java
+++ Chapter 1/stringCompression.java
@@ -1,21 +1,26 @@
 public class stringCompression{
 	static String stringCompression(String str){
-		String cStr = "";
+		StringBuilder cStr = new StringBuilder();
 		int count = 0;
 		for(int i=0; i<str.length(); i++){
 			count++;
 			if(i+1==str.length() || str.charAt(i+1)!=str.charAt(i)){
-				cStr += "" + str.charAt(i) + count;
+				cStr.append(str.charAt(i));
+				cStr.append(count);
 				count = 0;
 			}
 		}
 		if(cStr.length()>str.length())
 			return str;
-		return cStr;
+		return cStr.toString();
 	}
 
 
+	static String stringCompressionAlternate(String str){
+		
+	}
+
 	public static void main(String[] args){
-		System.out.println(stringCompression("benmier"));
+		System.out.println(stringCompressionAlternate("aaabcccccdd"));
 	}
 }
