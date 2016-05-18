47094fe093da4930146f4b6b28289264408e95a0
benmier <bencmier@gmail.com>
2016-05-18 10:24:28 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/rotateMatrix.java
+++ Chapter 1/rotateMatrix.java
@@ -1,18 +1,19 @@
 public class rotateMatrix{
-	static int[][] rotateMatrix(int[][] arr){
-		for(int i: arr){
-			for(int j: arr[i]){
-				
-			}
-		}
+	// static int[][] rotateMatrix(int[][] arr){
+	// 	for(int i: arr){
+	// 		for(int j: arr[i]){
+	// 			int temp = arr[i][j];
+	// 			arr[i][j] = arr[i];
+	// 		}
+	// 	}
 
-		return arr;		
-	}
+	// 	return arr;		
+	// }
 
 	public static void main(String[] args){
 		int test[][] = {{1,1,1},{2,2,2},{3,3,3},{4,4,4,4}};
-		System.out.println(java.util.Arrays.deepToString(test));
-		rotateMatrix(test);
+		System.out.println(test.length);
+		// rotateMatrix(test);
 		System.out.println(java.util.Arrays.deepToString(test));
 	}
 }
