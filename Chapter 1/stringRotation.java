9662e2e2b189a648083e385216cd46eff35d6eb6
benmier <bencmier@gmail.com>
2016-05-18 15:16:06 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/stringRotation.java
+++ Chapter 1/stringRotation.java
@@ -2,9 +2,16 @@ public class stringRotation{
 	static boolean stringRotation(String s1, String s2){
 		if(s1.length()!=s2.length())
 			return false;
+		int s1Index = 0;
 		for(int i=0; i<s2.length(); i++){
 			if(s2[i]==s1[0]){
-				while(if(s2[i+1]==s1[1])
+				int j = i+1;
+				while(j<s2.length()){
+					if(s2[j]!=s1[s1Index++])
+						return false;
+					j++;
+				}
+
 			}
 		}
 		return false;
