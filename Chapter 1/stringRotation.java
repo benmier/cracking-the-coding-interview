5b43c945ae74f93b6bf301e180773762fedff9cb
benmier <bencmier@gmail.com>
2016-05-19 10:02:48 -0700

    Cracking the Coding Interview Algorithms

--- Chapter 1/stringRotation.java
+++ Chapter 1/stringRotation.java
@@ -1,6 +1,6 @@
 public class stringRotation{
 	static boolean stringRotation(String s1, String s2){
-		if(s1.length()!=s2.length())
+		if(s1.length()!=s2.length() || s1.length()==0)
 			return false;
 		for(int i=0; i<s2.length(); i++){
 			int s1Index = 0;
@@ -22,7 +22,15 @@ public class stringRotation{
 		return false;
 	}
 
-
+	static boolean stringRotationAlternate(String s1, String s2){
+		if(s1.length()!=s2.length() || s1.length()==0)
+			return false;
+		else{
+			if(isSubstring(s1+s1,s2))
+				return true;
+			return false;
+		}
+	}
 
 	public static boolean isSubstring(String big, String small) {
 		if (big.indexOf(small) >= 0) {
@@ -33,6 +41,6 @@ public class stringRotation{
 	}
 
 	public static void main(String[] args){
-		System.out.println(stringRotation("waterbottle","bottlewater"));
+		System.out.println(stringRotationAlternate("waterbottle","bottlewater"));
 	}
 }
