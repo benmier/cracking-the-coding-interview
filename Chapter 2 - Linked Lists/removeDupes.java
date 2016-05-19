0c5b689f0b438ea14c7d481ad0762175d081db11
benmier <bencmier@gmail.com>
2016-05-19 14:48:46 -0700

    Cracking the Coding Interview Algorithms Chapter 2

--- Chapter 2 - Linked Lists/removeDupes.java
+++ Chapter 2 - Linked Lists/removeDupes.java
@@ -1,9 +1,15 @@
 public class removeDupes{
-	static removeDupes(Node head){
-		
-	}
+	// static removeDupes(Node head){
+
+	// }
 
 	public static void main(String[] args){
+		Node head = createList(10);
+		head.printList();
+		// removeDupes(head);
+	}
+
+	static Node createList(int length){
 		Node head = new Node(0);
 		Node first = head;
 		for (int i=1; i<10; i++) {
@@ -11,19 +17,7 @@ public class removeDupes{
 			first.next = second;
 			first = second;
 		}
-		printList(head,10);
-		removeDupes(head);
-	}
-
-	static void printList(Node head, int length){
-		Node curr = head;
-		for(int j=0; j<length; j++){
-			if(curr.next==null)
-				System.out.println(curr.data);
-			else
-				System.out.print(curr.data+", ");
-			curr = curr.next;
-		}
+		return head;
 	}
 }
 
@@ -45,5 +39,31 @@ class Node{
 			n = n.next;
 		}
 		n.next = end;
+	}
+
+	Node deleteNode(Node head, int d){
+		Node n = head;
+		if(n.data==d){
+			return head.next;
+		}
+		while(n.next!=null){
+			if(n.next.data==d){
+				n.next = n.next.next;
+				return head;
+			}
+			n = n.next;
+		}
+		return head;
+	}
+
+	public void printList(){
+		Node curr = head;
+		while(curr.next.next!=null){
+			if(curr.next==null)
+				System.out.println(curr.data);
+			else
+				System.out.print(curr.data+", ");
+			curr = curr.next;
+		}
 	}
 }
