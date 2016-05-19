158821d2ebd7ea48607af04d2d68db854e1d79f9
benmier <bencmier@gmail.com>
2016-05-19 10:03:49 -0700

    Cracking the Coding Interview Algorithms Chapter 2

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 for i in {1..144}
 do
 	git add .
-	git commit -m 'Cracking the Coding Interview Algorithms'
+	git commit -m 'Cracking the Coding Interview Algorithms Chapter 2'
 	git push
 	sleep 300
 done
