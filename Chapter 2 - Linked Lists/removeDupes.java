d59372d4026af16d71319ed86ae41e53cb63f6b0
benmier <bencmier@gmail.com>
2016-05-19 14:28:27 -0700

    Cracking the Coding Interview Algorithms Chapter 2

--- Chapter 2 - Linked Lists/removeDupes.java
+++ Chapter 2 - Linked Lists/removeDupes.java
@@ -1,3 +1,29 @@
+public class removeDupes{
+
+
+	public static void main(String[] args){
+		Node first = new Node(0);
+		Node head = first;
+		Node second = first;
+		for (int i = 1; i < 8; i++) {
+			second = new Node(i);
+			first.next = second;
+			first = second;
+		}
+		Node curr = head;
+		for(int j=0; j<8; j++){
+			if(curr.next==null)
+				System.out.println(curr.data);
+			else
+				System.out.print(curr.data+", ");
+			curr = curr.next;
+		}
+	}
+}
+
+
+
+
 class Node{
 	Node next = null;
 	int data;
@@ -13,21 +39,5 @@ class Node{
 			n = n.next;
 		}
 		n.next = end;
-	}
-}
-
-public class removeDupes{
-
-
-	public static void main(String[] args){
-		Node first = new Node(0);
-		Node head = first;
-		Node second = first;
-		for (int i = 1; i < 8; i++) {
-			second = new Node(i);
-			first.next = second;
-			first = second;
-		}
-		System.out.println(head.printForward());
 	}
 }
