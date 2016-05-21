700dcb2bde3e02cf539a86073c68b1c9bcb52c3d
benmier <bencmier@gmail.com>
2016-05-21 14:53:54 -0700

    Cracking the Coding Interview Algorithms Chapter 2

---
+++ Chapter 2 - Linked Lists/palindrome.java
@@ -0,0 +1,65 @@
+public class palindrome{
+	static boolean palindrome(Node n){
+		
+	}
+
+	public static void main(String[] args){
+		char[] info = {"r","a","c","e","c","a","r"};
+		Node sLL = createList(info);
+		printList(sLL);
+		System.out.println(palindrome(sLL));
+	}
+
+	static Node createList(int[] info){
+		Node head = new Node(0);
+		Node first = head;
+		for (int i: info) {
+			Node second = new Node(i);
+			first.next = second;
+			first = second;
+		}
+		return head;
+	}
+
+	static void printList(Node head){
+		Node curr = head;
+		while(curr.next!=null){
+			System.out.print(curr.data+", ");
+			curr = curr.next;
+		}
+		System.out.println(curr.data);
+		return;
+	}
+
+	static Node deleteNode(Node head, int d){
+		Node n = head;
+		if(n.data==d)
+			return head.next;
+		while(n.next!=null){
+			if(n.next.data==d){
+				n.next = n.next.next;
+				return head;
+			}
+			n = n.next;
+		}
+		return head;
+	}
+}
+
+class Node{
+	Node next = null;
+	int data;
+
+	public Node(int d){
+		data = d;
+	}
+
+	void appendToTail(int d){
+		Node end = new Node(d);
+		Node n = this;
+		while(n.next!=null){
+			n = n.next;
+		}
+		n.next = end;
+	}
+}
