a355cfea74312a282207de6611fe7d8039782ac0
benmier <bencmier@gmail.com>
2016-05-21 11:58:50 -0700

    Cracking the Coding Interview Algorithms Chapter 2

---
+++ Chapter 2 - Linked Lists/partition.java
@@ -0,0 +1,67 @@
+public class partition{
+	static void partition(Node n){
+		
+	}
+
+
+	public static void main(String[] args){
+		int[] info = {1,2,3,4,5,6,7,8,9};
+		Node sLL = createList(info);
+		printList(sLL);
+		partition(sLL.next.next.next.next.next);
+		printList(sLL);
+	}
+
+	static Node createList(int[] info){
+		Node head = new Node(0);
+		Node first = head;
+		for (int i: info) {
+			Node second = new Node(i);
+			first.next = second;
+			first = second;
+		}
+		return head;
+	}
+
+	static void printList(Node head){
+		Node curr = head;
+		while(curr.next!=null){
+			System.out.print(curr.data+", ");
+			curr = curr.next;
+		}
+		System.out.println(curr.data);
+		return;
+	}
+
+	static Node deleteNode(Node head, int d){
+		Node n = head;
+		if(n.data==d)
+			return head.next;
+		while(n.next!=null){
+			if(n.next.data==d){
+				n.next = n.next.next;
+				return head;
+			}
+			n = n.next;
+		}
+		return head;
+	}
+}
+
+class Node{
+	Node next = null;
+	int data;
+
+	public Node(int d){
+		data = d;
+	}
+
+	void appendToTail(int d){
+		Node end = new Node(d);
+		Node n = this;
+		while(n.next!=null){
+			n = n.next;
+		}
+		n.next = end;
+	}
+}
