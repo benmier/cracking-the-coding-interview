bf9f69b62b38a9498c4e0709900c4f6affe2515c
benmier <bencmier@gmail.com>
2016-05-22 13:21:05 -0700

    Cracking the Coding Interview Algorithms Chapter 2

--- Chapter 2 - Linked Lists/palindrome.java
+++ Chapter 2 - Linked Lists/palindrome.java
@@ -18,11 +18,39 @@ public class palindrome{
 		return true;
 	}
 
+	static Node palindromeAlternate(Node n){
+		Node reversed = reverseAndClone(n);
+		return reversed;
+	}
+
+		static Node reverseAndClone(Node n){
+			Node head = null;
+			while(n!=null){
+				Node n2 = new Node(n.data);
+				n.next = head;
+				head = n2;
+				n = n.next;
+			}
+			return head;
+		}
+
+		static boolean isEqual(Node n1, Node n2){
+			while(n1!=null && n2!=null){
+				if(n1.data!=n2.data)
+					return false;
+				n1 = n1.next;
+				n2 = n2.next;
+			}
+			return n1==null && n2==null;
+		}
+
 	public static void main(String[] args){
 		int[] info = {0,1,2,4,3,2,1,0};
 		Node sLL = createList(info);
 		printList(sLL);
-		System.out.println(palindrome(sLL));
+		System.out.println(palindromeAlternate(sLL));
+		Node sLL2 = palindromeAlternate(sLL);
+		printList(sLL2);
 	}
 
 	static Node createList(int[] info){
