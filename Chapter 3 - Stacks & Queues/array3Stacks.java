16361082c82c65762378e4ead77299f5172bafd7
benmier <bencmier@gmail.com>
2016-05-24 09:10:35 -0700

    Cracking the Coding Interview Algorithms Chapter 3

--- Chapter 3 - Stacks & Queues/array3Stacks.java
+++ Chapter 3 - Stacks & Queues/array3Stacks.java
@@ -19,7 +19,35 @@ public class array3Stacks{
 		values[indexOfTop(stackNum)] = value;
 	}
 
-	public int pop()
+	public int pop(int stackNum){
+		if(isEmpty(stackNum))
+			throw new EmptyStackException();
+		int topIndex = indexOfTop(stackNum); 
+		int value = values[topIndex]; //Get top
+		values[topIndex] = 0; //Clear
+		sizes[stackNum]--; //Shrink
+		return value;
+	}
+
+	public int peek(int stackNum){
+		if(isEmpty(stackNum))
+			throw new EmptyStackException();
+		return values[indexOfTop(stackNum)];
+	}
+
+	public boolean isEmpty(int stackNum){
+		return sizes[stackNum] == 0;
+	}
+
+	public boolean isFull(int stackNum){
+		return sizes[stackNum] == stackCapacity;
+	}
+
+	private int indexOfTop(int stackNum){
+		int offset = stackNum * stackCapacity;
+		int size = sizes[stackNum];
+		return offset + size - 1;
+	}
 
 
 	public static void main(String[] args){
