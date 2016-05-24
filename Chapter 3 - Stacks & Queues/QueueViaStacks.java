7628b0669589f4d592eb0312063f597a4c90cdf7
benmier <bencmier@gmail.com>
2016-05-24 14:53:24 -0700

    Cracking the Coding Interview Algorithms Chapter 3

--- Chapter 3 - Stacks & Queues/QueueViaStacks.java
+++ Chapter 3 - Stacks & Queues/QueueViaStacks.java
@@ -2,12 +2,30 @@ import java.util.*;
 
 public class QueueViaStacks{
 	public static void main(String [] args){
-		Queue queue = new Queue;
+		Queue queue = new Queue();
+		queue.push(0);
+		queue.push(1);
 	}
 }
 
 class Queue{
 	Stack s1 = new Stack();
 	Stack s2 = new Stack();
-	
+
+	void push(int value){
+		s1.push(value);
+		System.out.println("Pushed "+s1.peek());
+	}
+
+	void peek(){
+		if(s2.isEmpty()){
+			while(s2!=null){
+				s2.push(s1.pop());
+			}
+		}
+		else
+			return s2.peek();
+	}
+
+	void pop()
 }
