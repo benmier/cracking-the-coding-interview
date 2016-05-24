332fdbc54c019184de2c3d4e3dcf218a0c3aa5b8
benmier <bencmier@gmail.com>
2016-05-24 09:05:30 -0700

    Cracking the Coding Interview Algorithms Chapter 3

--- Chapter 3 - Stacks & Queues/array3Stacks.java
+++ Chapter 3 - Stacks & Queues/array3Stacks.java
@@ -1,7 +1,26 @@
 import java.util.*;
 
 public class array3Stacks{
-	 
+	private int numberOfStacks = 3;
+	private int stackCapacity;
+	private int[] values;
+	private int[] sizes;
+
+	public FixedMultiStack(int stackSize){
+		stackCapacity = stackSize;
+		values = new int[stackSize*numberOfStacks];
+		sizes = new int[numberOfStacks];
+	}
+
+	public void push(int stackNum, int value) throws FullStackException{
+		if(isFull(stackNum))
+			throw new FullStackException();
+		sizes[stackNum]++;
+		values[indexOfTop(stackNum)] = value;
+	}
+
+	public int pop()
+
 
 	public static void main(String[] args){
 		int[] info = {0,1,2,4,2,1,0};
