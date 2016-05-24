8116a41b512ed3efc25ea8e72836aa823aa9bf67
benmier <bencmier@gmail.com>
2016-05-24 15:28:59 -0700

    Cracking the Coding Interview Algorithms Chapter 3

---
+++ Chapter 3 - Stacks & Queues/SortStack.java
@@ -0,0 +1,19 @@
+public class SortStack{
+	static sortStack(Stack s){
+
+	}
+
+	public static void main(String [] args){
+		Stack stack = new Queue();
+		stack.push(9);
+		stack.push(1);
+		stack.push(8);
+		stack.push(2);
+		stack.push(7);
+		stack.push(3);
+		stack.push(6);
+		stack.push(4);
+		stack.push(5);
+		sortStack(stack);
+	}
+}
