8edae91b0ec4358d57a20973e70e9dcc19128375
benmier <bencmier@gmail.com>
2016-05-24 12:44:15 -0700

    Cracking the Coding Interview Algorithms Chapter 3

--- Chapter 3 - Stacks & Queues/StackOfPlates.java
+++ Chapter 3 - Stacks & Queues/StackOfPlates.java
@@ -2,22 +2,24 @@ import java.util.*;
 
 public class StackOfPlates{
 	public static void main(String [] args){
-		SetOfStacks stack = new SetOfStacks();
+		SetOfStacks stack = new SetOfStacks(3);
 		
 	}
 }
 
-public class SetOfStacks{
+class SetOfStacks{
 	private int stackCapacity;
 	private int size;
 	private int currentStack = 0;
 	private ArrayList<Stack> stacks = new ArrayList<Stack>();
+	private Stack first = new Stack();
+	stacks.add(first);
 	public SetOfStacks(int size){
 		stackCapacity = size;
 	}
 	public void push(int value){
 		if(size<=stackCapacity){
-			super.push();
+			stacks[0].push(value);
 			size++;
 		}
 		else
