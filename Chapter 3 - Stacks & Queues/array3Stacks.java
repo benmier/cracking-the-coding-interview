4abe4b4589656e8be1ef211986b2d4eb1214a192
benmier <bencmier@gmail.com>
2016-05-24 09:15:40 -0700

    Cracking the Coding Interview Algorithms Chapter 3

--- Chapter 3 - Stacks & Queues/array3Stacks.java
+++ Chapter 3 - Stacks & Queues/array3Stacks.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import FullStackException;
 
 public class array3Stacks{
 	private int numberOfStacks = 3;
@@ -6,7 +7,7 @@ public class array3Stacks{
 	private int[] values;
 	private int[] sizes;
 
-	public FixedMultiStack(int stackSize){
+	public array3Stacks(int stackSize){
 		stackCapacity = stackSize;
 		values = new int[stackSize*numberOfStacks];
 		sizes = new int[numberOfStacks];
@@ -50,10 +51,41 @@ public class array3Stacks{
 	}
 
 
-	public static void main(String[] args){
-		int[] info = {0,1,2,4,2,1,0};
-		Node sLL = createList(info);
-		printList(sLL);
-		System.out.println(loopDetection(sLL));
+	public static void printStacks(array3Stacks stacks) {
+		System.out.println(AssortedMethods.arrayToString(stacks.getValues()));
+	}
+	
+	public static void main(String [] args) throws Exception  {
+		array3Stacks stacks = new array3Stacks(4);
+		printStacks(stacks);
+		stacks.push(0, 10);
+		printStacks(stacks);
+		stacks.push(1, 20);
+		printStacks(stacks);
+		stacks.push(2, 30);
+		printStacks(stacks);
+		
+		stacks.push(1, 21);
+		printStacks(stacks);
+		stacks.push(0, 11);
+		printStacks(stacks);
+		stacks.push(0, 12);
+		printStacks(stacks);
+		
+		stacks.pop(0);
+		printStacks(stacks);
+		
+		stacks.push(2, 31);
+		printStacks(stacks);
+		
+		stacks.push(0, 13);
+		printStacks(stacks);
+		stacks.push(1, 22);
+		printStacks(stacks);
+		
+		stacks.push(2, 31);
+		printStacks(stacks);
+		stacks.push(2, 32);
+		printStacks(stacks);
 	}
 } 
