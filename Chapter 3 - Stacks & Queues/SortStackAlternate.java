4b217e8946f2564366a2a7bc71ce707c055d4577
benmier <bencmier@gmail.com>
2016-05-24 18:28:08 -0700

    Cracking the Coding Interview Algorithms Chapter 3

--- Chapter 3 - Stacks & Queues/SortStackAlternate.java
+++ Chapter 3 - Stacks & Queues/SortStackAlternate.java
@@ -1,22 +1,18 @@
 public class SortStackAlternate{
 	static void sortStack(Stack unsorted){
 		Stack buffer = new Stack();
-		System.out.println("Popped "+unsorted.peek()+" from unsorted into buffer");
 		buffer.push(unsorted.pop());
 		while(!unsorted.isEmpty()){
 			int value = unsorted.pop();
-			System.out.println("Value is now "+buffer.peek());
 			int count = 0;
 			while(!buffer.isEmpty() && value<buffer.peek()){
-				System.out.println("Popped "+buffer.peek()+" from buffer into unsorted");
 				unsorted.push(buffer.pop());
 				count++;
 			}
-			System.out.println("New min! Pushed "+value+" into buffer");
 			buffer.push(value);
-			// for(int i=0; i<count; i++){
-			// 	buffer.push(unsorted.pop());
-			// }
+			for(int i=0; i<count; i++){
+				buffer.push(unsorted.pop());
+			}
 		}
 		while(!buffer.isEmpty()){
 			unsorted.push(buffer.pop());
@@ -33,8 +29,14 @@ public class SortStackAlternate{
 	public static void main(String [] args){
 		Stack stack = new Stack();
 		stack.push(1);
+		stack.push(9);
 		stack.push(2);
+		stack.push(8);
 		stack.push(3);
+		stack.push(7);
+		stack.push(4);
+		stack.push(6);
+		stack.push(5);
 		sortStack(stack);
 		printStack(stack);
 	}
