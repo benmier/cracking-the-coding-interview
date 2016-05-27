c5e33962399c7161e2702006d88e1cb7c95d9d28
benmier <bencmier@gmail.com>
2016-05-26 18:53:51 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/BinarySearchTree.java
+++ Chapter 4 - Trees & Graphs/BinarySearchTree.java
@@ -4,15 +4,26 @@ public class BinarySearchTree{
 
 	public static void main(String [] args){
 		int[] array = {0,1,2,3,4,5,6,7,8,9};
-		makeTree(array, 0, array.length-1);
+		Node tree = makeTree(array, 0, array.length-1);
+		inOrderTraversal(tree);
 	}
 
 	public static Node makeTree(int[] arr, int start, int end){
 		if(start<end)
 			return null;
 		int mid = (start+end)/2;
+		Node n = new Node(arr[mid]);
+		n.left = makeTree(arr,start,mid-1);
+		n.right = makeTree(arr,mid+1,end);
+		return n;
+	}
 
-		makeTree()
+	public static void inOrderTraversal(Node node){
+		if(node!=null){
+			inOrderTraversal(node.left);
+			System.out.print(node.name+" ");
+			inOrderTraversal(node.right);
+		}
 	}
 }
 
