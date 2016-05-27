f5ba17c942a7977bcd72095b3a060ea2cea86316
benmier <bencmier@gmail.com>
2016-05-27 15:03:02 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/checkBalancedAlternate.java
+++ Chapter 4 - Trees & Graphs/checkBalancedAlternate.java
@@ -26,7 +26,7 @@ public class checkBalancedAlternate{
 	}
 
 	public static void main(String [] args){
-		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0};
 		Node tree = makeTree(array, 0, array.length-1);
 		System.out.println(isBalanced(tree));
 	}
---
+++ Chapter 4 - Trees & Graphs/isBST.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class isBST{
+
+	public static int isBST(Node root){
+		
+	}
+
+
+	public static void main(String [] args){
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0};
+		Node tree = makeTree(array, 0, array.length-1);
+		System.out.println(isBalanced(tree));
+	}
+
+	public static Node makeTree(int[] arr, int start, int end){
+		if(start>end)
+			return null;
+		int mid = (start+end)/2;
+		Node n = new Node(arr[mid]);
+		n.left = makeTree(arr,start,mid-1);
+		n.right = makeTree(arr,mid+1,end);
+		return n;
+	}
+}
