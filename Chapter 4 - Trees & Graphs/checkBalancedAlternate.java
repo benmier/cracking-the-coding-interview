3ba3f82a34f4d30a14984e54f52a39e7c494a528
benmier <bencmier@gmail.com>
2016-05-27 14:32:32 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/checkBalancedAlternate.java
+++ Chapter 4 - Trees & Graphs/checkBalancedAlternate.java
@@ -2,14 +2,32 @@ import java.util.*;
 
 public class checkBalancedAlternate{
 
-	public static boolean checkBalanced(Node node, int leftHeight, int rightHeight){
-		
+	public static int checkHeight(Node root){
+		if(root==null)
+			return -1;
+
+		int leftHeight = checkHeight(root.left);
+		if(leftHeight==Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+
+		int rightHeight = checkHeight(root.right);
+		if(rightHeight==Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+
+		if(Math.abs(leftHeight-rightHeight)>1)
+			return Integer.MIN_VALUE;
+		else
+			return Math.max(leftHeight,rightHeight)+1;
+	}
+
+	static boolean isBalanced(Node root){
+		return checkHeight(root)!=Integer.MIN_VALUE;
 	}
 
 	public static void main(String [] args){
 		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
 		Node tree = makeTree(array, 0, array.length-1);
-		System.out.println(checkBalanced(tree,0,0));
+		System.out.println(isBalanced(tree));
 	}
 
 	public static Node makeTree(int[] arr, int start, int end){
