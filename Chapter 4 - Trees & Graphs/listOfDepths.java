1cc2ef4068a192b70d7f0b1b092755da06e36727
benmier <bencmier@gmail.com>
2016-05-27 11:48:35 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/listOfDepths.java
+++ Chapter 4 - Trees & Graphs/listOfDepths.java
@@ -4,21 +4,23 @@ public class listOfDepths{
 
 	public static void listOfDepths(Node node, ArrayList<LinkedList<Node>> lists, int depth){
 		LinkedList<Node> list = null;
-		if(list.size()==level){
+		if(lists.size()==depth){
 			list = new LinkedList<Node>();
 			lists.add(list);
 		}
 		if(node!=null){
+			lists[depth].add(node);
 			System.out.println(node.name+" "+depth); 
-			listOfDepths(node.left,depth+1);
-			listOfDepths(node.right,depth+1);
+			listOfDepths(node.left,lists,depth+1);
+			listOfDepths(node.right,lists,depth+1);
 		}
 	}
 
 	public static void main(String [] args){
 		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
 		Node tree = makeTree(array, 0, array.length-1);
-		listOfDepths(tree,0);
+		LinkedList<Node> lists = new LinkedList<Node>();
+		listOfDepths(tree,lists,0);
 		System.out.println("");
 	}
 
