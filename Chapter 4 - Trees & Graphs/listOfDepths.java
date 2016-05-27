5a50125684df90f399dffa372c900b6392530fd1
benmier <bencmier@gmail.com>
2016-05-27 11:22:53 -0700

    Cracking the Coding Interview Algorithms Chapter 4

---
+++ Chapter 4 - Trees & Graphs/listOfDepths.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class listOfDepths{
+
+	public static Node[] listOfDepths(Node tree){
+		
+	}
+
+	public static void main(String [] args){
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
+		Node tree = makeTree(array, 0, array.length-1);
+		System.out.println("");
+		inOrderTraversal(tree);
+	}
+
+	public static Node makeTree(int[] arr, int start, int end){
+		if(start>end)
+			return null;
+		int mid = (start+end)/2;
+		Node n = new Node(arr[mid]);
+		System.out.print(mid+" ");
+		n.left = makeTree(arr,start,mid-1);
+		n.right = makeTree(arr,mid+1,end);
+		return n;
+	}
+
+	public static void inOrderTraversal(Node node){
+		if(node!=null){
+			inOrderTraversal(node.left);
+			System.out.print(node.name+" ");
+			inOrderTraversal(node.right);
+		}
+	}
+}
