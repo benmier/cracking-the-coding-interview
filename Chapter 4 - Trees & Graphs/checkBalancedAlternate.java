2509e265973ea5eede9052169d9f7faf9c684054
benmier <bencmier@gmail.com>
2016-05-27 14:22:21 -0700

    Cracking the Coding Interview Algorithms Chapter 4

---
+++ Chapter 4 - Trees & Graphs/checkBalancedAlternate.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class checkBalancedAlternate{
+
+	public static boolean checkBalanced(Node node, int leftHeight, int rightHeight){
+		
+	}
+
+	public static void main(String [] args){
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
+		Node tree = makeTree(array, 0, array.length-1);
+		System.out.println(checkBalanced(tree,0,0));
+	}
+
+	public static Node makeTree(int[] arr, int start, int end){
+		if(start>end)
+			return null;
+		int mid = (start+end)/2;
+		Node n = new Node(arr[mid]);
+		n.left = makeTree(arr,start,mid-1);
+		n.right = makeTree(arr,mid+1,end);
+		return n;
+	}
+}
