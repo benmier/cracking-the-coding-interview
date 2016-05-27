ca9b3a52e354a4c492b6ea376bf1c941b18a5a30
benmier <bencmier@gmail.com>
2016-05-27 11:12:39 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/BinarySearchTree.java
+++ Chapter 4 - Trees & Graphs/BinarySearchTree.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class BinarySearchTree{
 
 	public static void main(String [] args){
-		int[] array = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
 		Node tree = makeTree(array, 0, array.length-1);
 		System.out.println("");
 		inOrderTraversal(tree);
@@ -14,9 +14,7 @@ public class BinarySearchTree{
 			return null;
 		int mid = (start+end)/2;
 		Node n = new Node(arr[mid]);
-		// System.out.println("Start is "+start);
-		// System.out.println("Mid is "+mid);
-		// System.out.println("End is "+end+"\n");
+		System.out.print(mid+" ");
 		n.left = makeTree(arr,start,mid-1);
 		n.right = makeTree(arr,mid+1,end);
 		return n;
