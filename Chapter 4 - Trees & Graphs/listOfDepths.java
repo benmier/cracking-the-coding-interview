057a4e9d188d072b60972ccb32ba6ea58296288f
benmier <bencmier@gmail.com>
2016-05-27 11:33:07 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/listOfDepths.java
+++ Chapter 4 - Trees & Graphs/listOfDepths.java
@@ -4,17 +4,17 @@ public class listOfDepths{
 
 	public static void listOfDepths(Node node, int depth){
 		if(node!=null){
-			System.out.print(node.name+" ");
-			listOfDepths(node.left);
-			listOfDepths(node.right);
+			System.out.println(node.name+" "+depth);
+			listOfDepths(node.left,depth+1);
+			listOfDepths(node.right,depth+1);
 		}
 	}
 
 	public static void main(String [] args){
 		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
 		Node tree = makeTree(array, 0, array.length-1);
-		System.out.println("");
 		listOfDepths(tree,0);
+		System.out.println("");
 	}
 
 	public static Node makeTree(int[] arr, int start, int end){
@@ -22,7 +22,7 @@ public class listOfDepths{
 			return null;
 		int mid = (start+end)/2;
 		Node n = new Node(arr[mid]);
-		System.out.print(mid+" ");
+		// System.out.print(mid+" ");
 		n.left = makeTree(arr,start,mid-1);
 		n.right = makeTree(arr,mid+1,end);
 		return n;
