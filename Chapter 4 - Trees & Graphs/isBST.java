60d3bae9d1543470f9ae238c41ffb0458a9086ba
benmier <bencmier@gmail.com>
2016-05-27 18:18:10 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/isBST.java
+++ Chapter 4 - Trees & Graphs/isBST.java
@@ -2,15 +2,22 @@ import java.util.*;
 
 public class isBST{
 
-	public static int isBST(Node root){
-		
+	public static boolean isBST(Node node, int curr){
+		if(node!=null){
+			isBST(node.left,curr);
+			if(curr<node.name)
+				return false;
+			System.out.print(node.name+" ");
+			isBST(node.right,curr);
+		}
+		return true;
 	}
 
 
 	public static void main(String [] args){
 		int[] array = {0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0};
 		Node tree = makeTree(array, 0, array.length-1);
-		System.out.println(isBalanced(tree));
+		System.out.println(isBST(tree,Integer.MAX_VALUE));
 	}
 
 	public static Node makeTree(int[] arr, int start, int end){
