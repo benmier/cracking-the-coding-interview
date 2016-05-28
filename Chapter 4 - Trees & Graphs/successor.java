8b2aab2aebb57081130b7a43a011b1b9ff3f53b2
benmier <bencmier@gmail.com>
2016-05-28 16:20:46 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 4 - Trees & Graphs/successor.java
+++ Chapter 4 - Trees & Graphs/successor.java
@@ -3,18 +3,18 @@ import java.util.*;
 public class successor{
 
 	public static int successor(Node node){
-		if(node.left!=null)
-			return node.left.name;
 		if(node.right!=null)
 			return node.right.name;
+		if(node.left!=null)
+			return node.left.name;
 		return 0;
 	}
 
 
 	public static void main(String [] args){
-		int[] array = {0,1,2,3,4,6,7,8,9,10};
+		int[] array = {0,1,2,3,4,5,6,7,8,9,10};
 		Node tree = makeTree(array, 0, array.length-1);
-		System.out.println(successor(tree.left));
+		System.out.println(successor(tree));
 	}
 
 	public static Node makeTree(int[] arr, int start, int end){
