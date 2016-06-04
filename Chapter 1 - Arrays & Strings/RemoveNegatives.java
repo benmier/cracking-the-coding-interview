ec7c139f2df37ef80a2f978ae6430e9c4895841d
benmier <bencmier@gmail.com>
2016-06-04 14:42:26 -0700

    Cracking the Coding Interview Algorithms Chapter 4

---
+++ Chapter 1 - Arrays & Strings/RemoveNegatives.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class RemoveNegatives{
+	public static void insertXatY(ArrayList arr, int num, int index){
+		arr.add(0);
+		for(int i=arr.size()-1; i>index; i--){
+			arr.set(i,arr.get(i-1));
+		}
+		arr.set(index,num);
+	}
+
+
+	public static void main(String [] args){
+		int[] add = {1,2,3,4};
+		ArrayList arr = new ArrayList();
+		arr.add(1);
+		arr.add(3);
+		arr.add(5);
+		arr.add(7);
+		System.out.println(arr);
+		insertXatY(arr,10,2);
+		System.out.println(arr);
+	}
+}
