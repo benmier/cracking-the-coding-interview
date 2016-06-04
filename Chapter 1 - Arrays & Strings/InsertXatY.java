db01fda7bec896f72b71fcfa777c4dfb9c364a37
benmier <bencmier@gmail.com>
2016-06-04 13:05:58 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 1 - Arrays & Strings/InsertXatY.java
+++ Chapter 1 - Arrays & Strings/InsertXatY.java
@@ -3,8 +3,14 @@ import java.util.*;
 public class InsertXatY{
 
 	public static void main(String [] args){
+		int[] add = {1,2,3,4};
 		ArrayList arr = new ArrayList();
-		arr.addAll(1,2,3,4,5);
-		// System.out.println();
+		arr.add(1);
+		arr.add(3);
+		arr.add(5);
+		arr.add(7);
+		System.out.println(arr);
+
+		
 	}
 }
