e591294a70eb73fe52e2af7199994de13fca8c49
benmier <bencmier@gmail.com>
2016-06-05 13:51:59 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- Chapter 1 - Arrays & Strings/BinarySearch.java
+++ Chapter 1 - Arrays & Strings/BinarySearch.java
@@ -3,18 +3,19 @@ import java.util.*;
 public class BinarySearch{
 	public static Integer binarySearch(int key, int[] array, int min, int max){
 		int mid = (max-min)/2;
+		System.out.println(array[mid]);
 		if(array[mid]==key)
 			return mid;
 		else if(array[mid]>key)
-			binarySearch(key, array, min, mid-1);
+			binarySearch(key, array, min, mid);
 		else
-			binarySearch(key, array, mid+1, max);
+			binarySearch(key, array, mid, max);
 		return null;
 	}
 
 
 	public static void main(String [] args){
 		int[] array = {-90,-19,0,2,12,29,33,190,320};
-		System.out.println(binarySearch(12, array, 0, array.length-1));
+		System.out.println(binarySearch(0, array, 0, array.length-1));
 	}
 }
