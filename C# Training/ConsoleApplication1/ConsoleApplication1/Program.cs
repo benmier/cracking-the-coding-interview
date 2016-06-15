95e4706171972ce94682fcee135d048a595a4a05
benmier <bencmier@gmail.com>
2016-06-15 14:41:37 -0700

    Cracking the Coding Interview Algorithms Chapter 4

---
+++ C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's yo name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
