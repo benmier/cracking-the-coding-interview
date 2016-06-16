3f97304119718b34b65c25ff4ed53e3c7a129a53
benmier <bencmier@gmail.com>
2016-06-15 22:44:14 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -56,15 +56,17 @@ namespace ConsoleApplication1
             double number1 = 10.5;
             double number2 = 15;
 
-            Console.WriteLine("Math.Abs(number1) " + (Math.Abs(number1)));
-            Console.WriteLine("Math.Ceiling(number1) " + (Math.Ceiling(number1)));
-            Console.WriteLine("Math.Floor(number1) " + (Math.Floor(number1)));
-            Console.WriteLine("Math.Max(number1, number2) " + (Math.Max(number1, number2)));
-            Console.WriteLine("Math.Min(number1, number2) " + (Math.Min(number1, number2)));
-            Console.WriteLine("Math.Pow(number1, 2) " + (Math.Pow(number1, 2)));
-            Console.WriteLine("Math.Round(number1) " + (Math.Round(number1)));
-            Console.WriteLine("Math.Sqrt(number1) " + (Math.Sqrt(number1)));
+            Console.WriteLine("Math.Abs(number1) =" + (Math.Abs(number1)));
+            Console.WriteLine("Math.Ceiling(number1) =" + (Math.Ceiling(number1)));
+            Console.WriteLine("Math.Floor(number1) =" + (Math.Floor(number1)));
+            Console.WriteLine("Math.Max(number1, number2) =" + (Math.Max(number1, number2)));
+            Console.WriteLine("Math.Min(number1, number2) =" + (Math.Min(number1, number2)));
+            Console.WriteLine("Math.Pow(number1, 2) =" + (Math.Pow(number1, 2)));
+            Console.WriteLine("Math.Round(number1) =" + (Math.Round(number1)));
+            Console.WriteLine("Math.Sqrt(number1) =" + (Math.Sqrt(number1)));
 
+            Random rand = new Random();
+            Console.WriteLine("Random Number between 1 and 10 is " + rand.Next(1,11));
         }
     }
 }
