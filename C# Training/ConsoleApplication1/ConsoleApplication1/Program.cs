21bd912b03e40cad493699d3f6090cbe5e6ce9d3
benmier <bencmier@gmail.com>
2016-06-15 22:39:10 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -33,21 +33,38 @@ namespace ConsoleApplication1
             //Console.WriteLine(newVAr);
             //Console.WriteLine("newVar is a {0}", newVAr.GetTypeCode());
 
-            Console.WriteLine("5 + 3 = " + (5 + 3));
-            Console.WriteLine("5 - 3 = " + (5 - 3));
-            Console.WriteLine("5 * 3 = " + (5 * 3));
-            Console.WriteLine("5 / 3 = " + (5 / 3));
-            Console.WriteLine("5.2 % 3 = " + (5.2 % 3));
-            int i = 0;
-            Console.WriteLine("i++ = " + (i++));
-            Console.WriteLine("++i = " + (++i));
-            Console.WriteLine("i-- = " + (i--));
-            Console.WriteLine("--i = " + (--i));
-            Console.WriteLine("i += 3 " + (i += 3));
-            Console.WriteLine("i -= 2 " + (i -= 2));
-            Console.WriteLine("i *= 2 " + (i *= 2));
-            Console.WriteLine("i /= 2 " + (i /= 2));
-            Console.WriteLine("i %= 2 " + (i %= 2));
+            //Console.WriteLine("5 + 3 = " + (5 + 3));
+            //Console.WriteLine("5 - 3 = " + (5 - 3));
+            //Console.WriteLine("5 * 3 = " + (5 * 3));
+            //Console.WriteLine("5 / 3 = " + (5 / 3));
+            //Console.WriteLine("5.2 % 3 = " + (5.2 % 3));
+            //int i = 0;
+            //Console.WriteLine("i++ = " + (i++));
+            //Console.WriteLine("++i = " + (++i));
+            //Console.WriteLine("i-- = " + (i--));
+            //Console.WriteLine("--i = " + (--i));
+            //Console.WriteLine("i += 3 " + (i += 3));
+            //Console.WriteLine("i -= 2 " + (i -= 2));
+            //Console.WriteLine("i *= 2 " + (i *= 2));
+            //Console.WriteLine("i /= 2 " + (i /= 2));
+            //Console.WriteLine("i %= 2 " + (i %= 2));
+
+            double pi = 3.14;
+            //int intPi = (int)pi;
+            //Console.WriteLine(pi + " "+intPi);
+
+            double number1 = 10.5;
+            double number2 = 15;
+
+            Console.WriteLine("Math.Abs(number1) " + (Math.Abs(number1)));
+            Console.WriteLine("Math.Ceiling(number1) " + (Math.Ceiling(number1)));
+            Console.WriteLine("Math.Floor(number1) " + (Math.Floor(number1)));
+            Console.WriteLine("Math.Max(number1, number2) " + (Math.Max(number1, number2)));
+            Console.WriteLine("Math.Min(number1, number2) " + (Math.Min(number1, number2)));
+            Console.WriteLine("Math.Pow(number1, 2) " + (Math.Pow(number1, 2)));
+            Console.WriteLine("Math.Round(number1) " + (Math.Round(number1)));
+            Console.WriteLine("Math.Sqrt(number1) " + (Math.Sqrt(number1)));
+
         }
     }
 }
