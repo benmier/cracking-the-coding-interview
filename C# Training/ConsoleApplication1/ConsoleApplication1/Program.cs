1a8973a760d1f46299c1c61186521921e6814211
benmier <bencmier@gmail.com>
2016-06-15 21:58:30 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -29,9 +29,25 @@ namespace ConsoleApplication1
             //Console.WriteLine(maxFloat);
             //Console.WriteLine(maxDouble);
 
-            var newVAr = "This is declared using var, and must now be a string";
+            var newVAr = "This is newVar, a string declared using var.";
             //Console.WriteLine(newVAr);
-            Console.WriteLine("newVar is a {0}", newVAr.GetTypeCode());
+            //Console.WriteLine("newVar is a {0}", newVAr.GetTypeCode());
+
+            Console.WriteLine("5 + 3 = " + (5 + 3));
+            Console.WriteLine("5 - 3 = " + (5 - 3));
+            Console.WriteLine("5 * 3 = " + (5 * 3));
+            Console.WriteLine("5 / 3 = " + (5 / 3));
+            Console.WriteLine("5.2 % 3 = " + (5.2 % 3));
+            int i = 0;
+            Console.WriteLine("i++ = " + (i++));
+            Console.WriteLine("++i = " + (++i));
+            Console.WriteLine("i-- = " + (i--));
+            Console.WriteLine("--i = " + (--i));
+            Console.WriteLine("i += 3 " + (i += 3));
+            Console.WriteLine("i -= 2 " + (i -= 2));
+            Console.WriteLine("i *= 2 " + (i *= 2));
+            Console.WriteLine("i /= 2 " + (i /= 2));
+            Console.WriteLine("i %= 2 " + (i %= 2));
         }
     }
 }
