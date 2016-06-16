56e1687fecc130192b04f66e3daf4cb7cb58e7ad
benmier <bencmier@gmail.com>
2016-06-15 22:49:18 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -38,7 +38,7 @@ namespace ConsoleApplication1
             //Console.WriteLine("5 * 3 = " + (5 * 3));
             //Console.WriteLine("5 / 3 = " + (5 / 3));
             //Console.WriteLine("5.2 % 3 = " + (5.2 % 3));
-            //int i = 0;
+            int i = 0;
             //Console.WriteLine("i++ = " + (i++));
             //Console.WriteLine("++i = " + (++i));
             //Console.WriteLine("i-- = " + (i--));
@@ -50,23 +50,37 @@ namespace ConsoleApplication1
             //Console.WriteLine("i %= 2 " + (i %= 2));
 
             double pi = 3.14;
-            //int intPi = (int)pi;
+            int intPi = (int)pi;
             //Console.WriteLine(pi + " "+intPi);
 
             double number1 = 10.5;
             double number2 = 15;
 
-            Console.WriteLine("Math.Abs(number1) =" + (Math.Abs(number1)));
-            Console.WriteLine("Math.Ceiling(number1) =" + (Math.Ceiling(number1)));
-            Console.WriteLine("Math.Floor(number1) =" + (Math.Floor(number1)));
-            Console.WriteLine("Math.Max(number1, number2) =" + (Math.Max(number1, number2)));
-            Console.WriteLine("Math.Min(number1, number2) =" + (Math.Min(number1, number2)));
-            Console.WriteLine("Math.Pow(number1, 2) =" + (Math.Pow(number1, 2)));
-            Console.WriteLine("Math.Round(number1) =" + (Math.Round(number1)));
-            Console.WriteLine("Math.Sqrt(number1) =" + (Math.Sqrt(number1)));
+            //Console.WriteLine("Math.Abs(number1) =" + (Math.Abs(number1)));
+            //Console.WriteLine("Math.Ceiling(number1) =" + (Math.Ceiling(number1)));
+            //Console.WriteLine("Math.Floor(number1) =" + (Math.Floor(number1)));
+            //Console.WriteLine("Math.Max(number1, number2) =" + (Math.Max(number1, number2)));
+            //Console.WriteLine("Math.Min(number1, number2) =" + (Math.Min(number1, number2)));
+            //Console.WriteLine("Math.Pow(number1, 2) =" + (Math.Pow(number1, 2)));
+            //Console.WriteLine("Math.Round(number1) =" + (Math.Round(number1)));
+            //Console.WriteLine("Math.Sqrt(number1) =" + (Math.Sqrt(number1)));
 
             Random rand = new Random();
-            Console.WriteLine("Random Number between 1 and 10 is " + rand.Next(1,11));
+            //Console.WriteLine("Random Number between 1 and 10 is " + rand.Next(1,11));
+
+            int age = 17;
+            if (age >= 5 && age <= 7)
+            {
+                Console.WriteLine("Elementary");
+            }
+            else if ((age > 7) && (age < 13))
+            {
+                Console.WriteLine("Middle");
+            }
+            else
+            {
+                Console.WriteLine("High");
+            }
         }
     }
 }
