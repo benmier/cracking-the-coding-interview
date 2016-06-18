81676817989b1378d2b13b7a9f12bf0ccc3cfff1
benmier <bencmier@gmail.com>
2016-06-17 21:18:57 -0700

    Cracking the Coding Interview Algorithms Chapter 4

--- C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C# Training/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -319,6 +320,24 @@ namespace ConsoleApplication1
             foreach(int num in oddNums)
             {
                 Console.WriteLine(num);
+            }
+
+            string[] custs = new string[] { "Tom", "Paul", "Greg" };
+            using (StreamWriter sw = new StreamWriter("custs.txt"))
+            {
+                foreach(string cust in custs)
+                {
+                    sw.WriteLine(cust);
+                }
+            }
+
+            string custName = "";
+            using(StreamReader sr = new StreamReader("custs.txt"))
+            {
+                while ((custName = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(custName);
+                }
             }
         }
     }
